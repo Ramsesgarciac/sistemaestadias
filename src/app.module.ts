9e535663d6f32fb2e649771dcf90b370542735f2
Ramsesgarciac <garciacuevasramses101@gmail.com>
2024-10-15 19:06:39 -0600

    que buena esta la hermana de gael gto y entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DireccionModule } from './direccion/direccion.module';
+import { SectorModule } from './sector/sector.module';
+import { EmpleadorModule } from './empleador/empleador.module';
+import { EmpresaModule } from './empresa/empresa.module';
+
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type:'mysql',
+    host:'localhost',
+    port:3306,
+    username:'root',
+    password:'',
+    database:'basesilla',
+    autoLoadEntities:true,
+    synchronize:true,
+  }), DireccionModule, SectorModule, EmpleadorModule, EmpresaModule, ],
 })
 export class AppModule {}
